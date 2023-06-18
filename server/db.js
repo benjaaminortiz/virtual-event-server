abf7b0861136021d5fd05dfb760fc303103aee60
Benjamin Ortiz <benjask89@icloud.com>
2023-06-18 19:09:32 -0300

    connection string

--- server/db.js
+++ server/db.js
@@ -1,11 +1,21 @@
-const Pool = require("pg").Pool
 
+import { Pool, Client } from 'pg'
+const connectionString = 'postgres://admin:V7fiAh3K0PlxWLBeK8kzpZuhsMkYn4Rm@dpg-ci7no5h8g3nfuca8atpg-a/attendant'
+ 
 const pool = new Pool({
-    user: "admin",
-    password: "V7fiAh3K0PlxWLBeK8kzpZuhsMkYn4Rm",
-    host: "dpg-ci7no5h8g3nfuca8atpg-a",
-    port: 5432,
-    database: "attendants"
-});
+  connectionString,
+})
+ 
+await pool.query('SELECT NOW()')
+await pool.end()
+ 
+const client = new Client({
+  connectionString,
+})
+ 
+await client.connect()
+ 
+await client.query('SELECT NOW()')
+ 
+await client.end()
 
-module.exports =  pool;

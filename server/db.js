81668d09df2a3bf1ed1370ef6dee279befa97405
Benjamin Ortiz <benjask89@icloud.com>
2023-06-18 18:56:58 -0300

    hosting

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user: "postgres",
-    password: "admin",
-    host: "localhost",
+    user: "admin",
+    password: "V7fiAh3K0PlxWLBeK8kzpZuhsMkYn4Rm",
+    host: "dpg-ci7no5h8g3nfuca8atpg-a",
     port: 5432,
-    database: "postgres"
+    database: "attendants"
 });
 
 module.exports =  pool;
--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./db")
 require("dotenv").config()
+const serverless = require('serverless-http')
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
 
 
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.json({ message: "Hello from server!" });
 
 });
@@ -35,7 +37,8 @@ app.post("/", async (req, res) => {
 
 });
 
+app.use( '/.netlify/server/api' , router)
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+ app.listen(PORT, () => {
+   console.log(`Server listening on ${PORT}`);
+ });
